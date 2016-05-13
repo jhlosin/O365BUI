ba9002474a4a42aca3748028edbcb27fb536fc9c
Peter Park <jhlosin@gmail.com>
2016-05-13 10:12:50 -0400

    added few functions

--- Office365Util.ps1
+++ Office365Util.ps1
@@ -1,3 +1,9 @@
+######################################
+#                                    #
+# Initial Connection to Office 365   # 
+#                                    #
+######################################
+
 function Connect-Office365
 {
     <#
@@ -11,10 +17,17 @@ function Connect-Office365
     connect-MsolService -credential $LiveCred
 }
 
+
+######################################
+#                                    #
+#       MSOL Services Util           # 
+#                                    #
+######################################
+
 function BulkNew-MsolUser {
     <#
 	.SYNOPSIS
-        It will create Windows Azure MsolUsers in bulk and modify attritbues if already exists.		
+        It will create Windows Azure MsolUsers in bulk and modify attritbues to the ones in the CSV if already exists.		
 	.EXAMPLE
 		PS> BulkNew-MsolUser -CsvLocation 'C:\AllUsers.csv'
 		
@@ -47,21 +60,7 @@ function BulkNew-MsolUser {
         $users = import-csv $CsvLocation
         # select a license
         $license = (Get-MsolAccountSku | Out-GridView -Title "Select the license to assign" -PassThru).AccountSkuId
-
-        $NewUserParams = @{
-            'UserPrincipalName' = $Username
-            'Name' = $Username
-            'GivenName' = $FirstName
-            'Surname' = $LastName
-            'Title' = $Title
-            'SamAccountName' = $Username
-            'AccountPassword' = (ConvertTo-SecureString $DefaultPassword -AsPlainText -Force)
-            'Enabled' = $true
-            'Initials' = $MiddleInitial
-            'Path' = "$Location,$DomainDn"
-            'ChangePasswordAtLogon' = $true
-        }		
-
+                
 		# for each user in users
         foreach ($user in $users)
         {
@@ -81,136 +80,59 @@ function BulkNew-MsolUser {
                 'postalCode' = $user.postalCode
                 'country' = $user.country
                 'UsageLocation' = $user.UsageLocation
-                'Manager' = $user.manager
-                'Company' = $user.Company
-
             }
 
-            $thisUser = Get-MsolUser -UserPrincipalName $user.primarySMTP -ErrorAction SilentlyContinue
+            $thisUser = Get-MsolUser -UserPrincipalName $UserParams.UserPrincipalName -ErrorAction SilentlyContinue
             # if thisUser does not exist
             if (!$thisUser) {
 	            # create a new msol user
                 Write-Host ("creating a new msoluser " + $UserParams.DisplayName)
                 New-MsolUser @UserParams
-
-                # set up other attritbutes
-                Set-User -Identity $UserParams.UserPrincipalName -Manager $UserParams.Manager -Company $UserParams.Company
 	        }
-        }
-	}
-}
 
-function BulkSet-MsolUser {
-    <#
-	.SYNOPSIS
-        It will create Windows Azure MsolUsers in bulk and modify attritbues if already exists.		
-	.EXAMPLE
-		PS> BulkSet-MsolUser -CsvLocation 'C:\AllUsers.csv'
-		
-	.PARAMETER CsvLocation
-	 	Location of the CSV file of users you want to import
-        ===List of Properties(column) in CSV===
-        FirstName(required)
-        LastName(required)
-        UserPrincipalName(PrimarySmtpAddress)(required)
-        Fax
-        Department
-        MobilePhone
-        OfficePhone
-        OfficeAddress
-        City
-        Title
-        State
-        PostalCode
-        Country
-        UsageLocation
-	#>
-    [CmdletBinding()]
-	param (
-		[parameter(Mandatory=$true)][string]$CsvLocation
-	)
-	process {
-        # import the user list csv to a variable users
-        $users = import-csv $CsvLocation
-        # select a license
-        $license = (Get-MsolAccountSku | Out-GridView -Title "Select the license to assign" -PassThru).AccountSkuId
-		
-		# for each user in users
-        foreach ($user in $users)
-        {
-            $FirstName = $user.FirstName
-            $LastName = $user.LastName
-            $fax = $user.fax
-            $PhoneNumber = $user.OfficePhone
-            $DisplayName =  $FirstName + " " + $LastName
-            $UserName = $user.PrimarySMTP
-            $StreetAddress = $user.OfficeAddress
-            $city = $user.city
-            $state = $user.state
-            $postalCode = $user.zip
-            $country = $user.country
-            $UsageLocation = $user.UsageLocation
-            $ForwardingTo = $user.ForwardingTo
-
-            $Manager = $user.Manager
-            $Company = $user.Company
-
-            $thisUser = Get-MsolUser -UserPrincipalName $user.primarySMTP -ErrorAction SilentlyContinue
-            # if thisUser does not exist
-            if (!$thisUser) {
-	            # create a new msol user
-                Write-Host "creating a new msoluser $DisplayName"
-                New-MsolUser -DisplayName $DisplayName -UserPrinciPalName $UserName `
-                    -FirstName $FirstName -LastName $LastName -Country $Country -City $city `
-                    -StreetAddress $StreetAddress -fax $fax -PhoneNumber $PhoneNumber -postalCode $postalCode `
-                    -UsageLocation $UsageLocation -LicenseAssignment $license -State $state
-
-                # set up other attritbutes
-                Set-User -Identity $thisUser.UserPrincipalName -Manager $Manager -Company $Company
-	        }
-    
-            # if alrady exist
+            # if already exist
             if ($thisUser) {
-                # try to update attributes.
-                Write-Host "The user $DisplayName already exists. trying to update attritbutes"
-                set-MsolUser -UserPrinciPalName $thisUser.userPrincipalName `
-                    -FirstName $FirstName -LastName $LastName -Country $Country -City $city `
-                    -StreetAddress $StreetAddress -fax $fax -PhoneNumber $PhoneNumber -postalCode $postalCode `
-                    -UsageLocation $UsageLocation -State $state
+                # update attributes.
+                Write-Host ("The user " + $UserParams.DisplayName + " already exists. trying to update attritbutes")
+                set-MsolUser @UserParams
         
                 # add the license you selected prior
                 Set-MsolUserLicense -UserPrincipalName $thisUser.UserPrincipalName -AddLicenses $license -ErrorAction SilentlyContinue
-
-                # set up forwarding and aliases
-                # TODO: need to create contacts first.
-                #Set-Mailbox -Identity $thisUser.userPrincipalName -ForwardingSmtpAddress $ForwardingTo
-
-                # set up other attritbutes
-                Set-User -Identity $thisUser.UserPrincipalName -Manager $Manager -Company $Company
-
-                
             }
+
+            # max company attribute is 64, cut it if it is longer than 64
+            if ($user.company.length -gt 64) {
+                ## HERE
+                $company = $user.manager.Substring(0,63)
+            }
+            # set up other attritbutes
+            Set-User -Identity $thisUser.UserPrincipalName -Manager $user.Manager -Company $company
+
         }
 	}
 }
 
+
 ######################################
 #                                    #
-#       Exchange Online Utils        # 
+#       Exchange Online Utils        #
 #                                    #
 ######################################
 
-function BulkEnable-inPlaceHold {
+function BulkNew-MailContact {
     <#
 	.SYNOPSIS
-        It will enable in-placehold for all users in a given CSV.
+        It will create mailcontact in bulk for all items in a given CSV.
 	.EXAMPLE
-		PS> BulkEnabl-inPlaceHold -CsvLocation 'C:\AllUsers.csv'
+		PS> BulkNew-MailContact -CsvLocation 'C:\conatcts.csv'
 		
 	.PARAMETER CsvLocation
 	 	Location of the CSV file of users you want to import
         ===List of Properties(column) in CSV===
-        PrimarySMTP(or alias)
+        name(required)
+        externalEmailAddress(required)
+        FirstName
+        Lastname
 	#>
     [CmdletBinding()]
 	param (
@@ -222,17 +144,64 @@ function BulkEnable-inPlaceHold {
         $users = import-csv $CsvLocation
 		
 		# for each user in users
-        foreach ($user in $users)
+        foreach ($item in $items)
         {
+            $thisUser = Get-MailContact $item.name -ErrorAction SilentlyContinue
+            if (!$thisUser) {
+                Write-Host ("creating " + $item.name)
+                New-MailContact -Name $item.name -ExternalEmailAddress $item.ExternalEmailAddress -DisplayName $item.name -FirstName $item.firstName -LastName $item.lastName
+            } else {
+                Write-Host ("The contact " + $item.name + " already exists. skipping...")
+            }
         }
 	}
 }
 
 
+function BulkSet-Mailbox {
+    <#
+	.SYNOPSIS
+        It will create mailcontact in bulk for all items in a given CSV.
+	.EXAMPLE
+		PS> BulkNew-MailContact -CsvLocation 'C:\conatcts.csv'
+		
+	.PARAMETER CsvLocation
+	 	Location of the CSV file of users you want to import
+        ===List of Properties(column) in CSV===
+        name(required)
+        externalEmailAddress(required)
+        FirstName
+        Lastname
+	#>
+    [CmdletBinding()]
+	param (
+		[parameter(Mandatory=$true)]
+        [string]$CsvLocation
+	)
+	process {
+        # import the user list csv to a variable users
+        $users = import-csv $CsvLocation
+		
+		# for each user in users
+        foreach ($item in $items)
+        {
+            $thisUser = Get-MailContact $item.name -ErrorAction SilentlyContinue
+            if (!$thisUser) {
+                Write-Host ("creating " + $item.name)
+                New-MailContact -Name $item.name -ExternalEmailAddress $item.ExternalEmailAddress -DisplayName $item.name -FirstName $item.firstName -LastName $item.lastName
+            } else {
+                Write-Host ("The contact " + $item.name + " already exists. skipping...")
+            }
+        }
+	}
+}
+
 #### TODO
 # forwarding
 # proxyaddresses
-# company - set-User
-# manager - set-User
 
 #enable in-place hold.
+#create contacts
+# set up forwarding and aliases
+
+#Set-Mailbox -Identity $thisUser.userPrincipalName -ForwardingSmtpAddress $ForwardingTo
